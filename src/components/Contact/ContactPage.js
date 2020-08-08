907369ee6899928ac237ca539c0f99d596b35b0c
Abinash Mohapatra <abinash@expensebit.com>
2020-08-08 13:20:06 +0530

    feedback form

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 class ContactPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            telNum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: ''
+        }
+    }
+
+    handleChange = (e) => {
+        const target = e.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Current State is: " + JSON.stringify(this.state));
+        alert("Current State is: " + JSON.stringify(this.state))
+    }
+
     render() {
         return (
             <div className="container text-white py-4">
@@ -37,13 +66,81 @@ class ContactPage extends Component {
                     <div className="col-12 col-sm-6 offset-sm-1">
                         <h5>Map of our Location</h5>
                     </div>
-                    {/* <div className="col-12 col-sm-11 offset-sm-1">
-                        <div className="btn-group" role="group">
-                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
-                        </div>
-                    </div> */}
+                </div>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send us Your Feedback</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup row>
+                                <Label htmlFor="firstName" md={2}>
+                                    First Name
+                                </Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstName" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastName" md={2}>
+                                    Last Name
+                                </Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastName" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="telNum" md={2}>
+                                    Contact Tel.
+                                </Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telNum" name="telNum" placeholder="Tel. Number" value={this.state.telNum} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>
+                                    Email
+                                </Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{ size: 6, offset: 2 }}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" name="agree" checked={this.state.agree} onChange={this.handleChange} /> {' '} <strong>May we contact you?</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={{ size: 3, offset: 1 }}>
+                                    <Input type="select" name="contactType" value={this.state.contactType} onChange={this.handleChange} >
+                                        <option>
+                                            Tel.
+                                        </option>
+                                        <option>
+                                            Email
+                                        </option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>
+                                    Your Feedback
+                                </Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message" rows="12" value={this.state.message} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{ size: 10, offset: 2 }}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
